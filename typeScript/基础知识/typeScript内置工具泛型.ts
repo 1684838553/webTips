ad9771a534a3756f95e130804779de47581255b0
江莹莹 <1684838553@qq.com>
2021-07-30 22:07:46 +0800

    ts常见关键字

--- typeScript/基础知识/typeScript内置工具泛型.ts
+++ typeScript/基础知识/typeScript内置工具泛型.ts
@@ -26,6 +26,119 @@ const user: IUser2 = {
 
 // type Exclude<T, U> = T extends U ? never : T;
 //4、Exclude T是定义的类型，U被排除掉被排除掉的数据
-// 可能只只用于常量
+// 可能只用于常量
 type TFoo = Exclude<1 | 2, 1 | 3>;
 const a: TFoo = 2;
+
+//Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+
+// 5、关键字 in
+enum Status {
+  "error",
+  "success",
+  "default",
+}
+
+//用于枚举类型
+type StatusMap = {
+  [key in Status]: string;
+};
+
+let state: StatusMap = {
+  0: "0",
+  1: "1",
+  2: "2",
+};
+
+//用于联合类型
+type Peoperty = "name" | "age" | "phone";
+
+type PeopertyObject = {
+  [key in Peoperty]: string;
+};
+
+// obj中的key值为Peoperty中所有值
+const obj: PeopertyObject = {
+  name: "",
+  age: "",
+  phone: "",
+};
+
+// 使用于基础类型
+type StringKey = {
+  [key in string]: string;
+};
+
+const param: StringKey = {
+  1: "name",
+};
+
+//等同于
+type StringKey1 = {
+  [key: string]: string;
+};
+const param1: StringKey1 = {
+  1: "name",
+};
+
+type IParams = {
+  name: string;
+  age: number;
+};
+
+type IParams1 = {
+  [key in keyof IParams]: IParams[key];
+};
+
+// 6 、typeof
+const value: number = 10;
+
+const value2: typeof value = 100;
+
+// const value3 = 10;
+
+// const value4: typeof value3 = 100;
+
+const data = {
+  value: 123,
+  text: "text",
+  subData: {
+    value: false,
+  },
+};
+
+type Data = typeof data;
+
+// 7、extends
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface Player extends Person {
+  // item: 'ball' | 'swing';
+}
+
+const A: Player = {
+  name: "",
+  age: 1,
+};
+
+// 8、infer
+
+type A1 = Array<string>;
+type MyFunc<T> = T extends infer U ? U : T;
+type a1 = MyFunc<A1>;
+
+type MyFunc1<T> = T extends (infer U)[] ? U : T; //解包，返回数组中的类型
+type a2 = MyFunc1<A1>;
+
+// 9、as
+type B1 = string | number;
+function foo(param: B1) {
+  return (param as string).indexOf("/") > -1;
+}
+
+function foo1(param: B1): param is string {
+  return typeof param === "string";
+}
