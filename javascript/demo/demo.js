a0b224ad1fb58bfc4c372d4941cb297adcf28b05
1684838553 <1684838553@qq.com>
2021-09-13 09:48:45 +0800

    d

--- javascript/demo/demo.js
+++ javascript/demo/demo.js
@@ -11,3 +11,49 @@ console.log(map.keys());
 map.forEach((key, value) => {
   console.log(key, value);
 });
+
+{
+  function Person(name, age) {
+    console.log(this, "this");
+    this.name = name;
+    this.age = age;
+
+    this.say = () => {
+      console.log(this.name + "的年纪" + this.age);
+    };
+  }
+  const p = new Person("tom", 12);
+  console.log(p.say());
+}
+
+{
+  function Person(name) {
+    this.name = name;
+    this.say = function () {
+      setTimeout(
+        function () {
+          console.log("hello " + this.name);
+        }.bind(this),
+        1000
+      );
+    };
+  }
+  var person = new Person("axuebin");
+  person.say();
+}
+
+{
+  Promise.resolve().then(function promise1() {
+    console.log("promise1");
+  });
+  setTimeout(function setTimeout1() {
+    console.log("setTimeout1");
+    Promise.resolve().then(function promise2() {
+      console.log("promise2");
+    });
+  }, 0);
+
+  setTimeout(function setTimeout2() {
+    console.log("setTimeout2");
+  }, 0);
+}
--- 前端题集整理/demo/变量.js
+++ 前端题集整理/demo/变量.js
@@ -1,80 +1,131 @@
-{
-  const arr = [1, 2];
-  // arr = [2, 3];
-  arr.push(2);
-  console.log(arr);
-  console.log(toString(undefined));
-}
+// {
+//   const arr = [1, 2];
+//   // arr = [2, 3];
+//   arr.push(2);
+//   console.log(arr);
+//   console.log(toString(undefined));
+// }
+
+// {
+//   var a = 10;
+//   var b = ++a;
+//   console.log(a, b);
+// }
+
+// {
+//   var a = 1;
+//   function foo() {
+//     var a = 2;
+//     function inner() {
+//       console.log(this.a);
+//     }
+//     inner();
+//   }
+
+//   foo();
+// }
+// {
+//   const str = " 111   11  ";
+//   console.log(str.trim());
+// }
+
+// {
+//   const list = [
+//     {
+//       name: "tom",
+//       value: 0,
+//     },
+//     {
+//       name: "tom1",
+//       value: 2,
+//     },
+//     {
+//       name: "tom2",
+//       value: 1,
+//     },
+//   ];
+
+//   const getSelectValue = (list, name) => {
+//     const data = list.filter((item) => item.name === name);
+//     if (data.length) {
+//       return data[0].value;
+//     } else {
+//       return name;
+//     }
+//   };
+
+//   console.log(getSelectValue(list, "tom"), getSelectValue(list, "pp"));
+// }
+// {
+//   var mySymbol = Symbol("oo");
+//   console.log(mySymbol);
+
+//   const obj = {
+//     name: "tom",
+//   };
+//   for (let i in obj) {
+//     console.log(i);
+//   }
+//   for (let i of obj) {
+//     console.log(i);
+//   }
+// }
+
+// {
+//   Number.prototype.say = () => {
+//     console.log("hello");
+//   };
+//   const a = 1;
+//   a.say();
+// }
 
 {
-  var a = 10;
-  var b = ++a;
-  console.log(a, b);
-}
+  //   const getYearNumber = (data) => {
+  //     if (data) {
+  //       const mun = data.getFullYear() - new Date()
+  //       return mun;
+  //     } else {
+  //       return 0;
+  //     }
+  //   };
 
-{
-  var a = 1;
-  function foo() {
-    var a = 2;
-    function inner() {
-      console.log(this.a);
-    }
-    inner();
-  }
+  //data +1
 
-  foo();
-}
-{
-  const str = " 111   11  ";
-  console.log(str.trim());
-}
+  function getAgeByPid(strBirthday) {
+    var birthYear = strBirthday.substr(0, 4);
+    var birthMonth = strBirthday.substr(4, 2);
+    var birthDay = strBirthday.substr(6, 2);
 
-{
-  const list = [
-    {
-      name: "tom",
-      value: 0,
-    },
-    {
-      name: "tom1",
-      value: 2,
-    },
-    {
-      name: "tom2",
-      value: 1,
-    },
-  ];
+    d = new Date();
+    var nowYear = d.getFullYear();
+    var nowMonth = d.getMonth() + 1;
+    var nowDay = d.getDate() - 1;
 
-  const getSelectValue = (list, name) => {
-    const data = list.filter((item) => item.name === name);
-    if (data.length) {
-      return data[0].value;
+    if (nowYear == birthYear) {
+      returnAge = 0; //同年 则为0岁
     } else {
-      return name;
+      var ageDiff = nowYear - birthYear; //年之差
+      if (ageDiff > 0) {
+        if (nowMonth == birthMonth) {
+          var dayDiff = nowDay - birthDay; //日之差
+          if (dayDiff < 0) {
+            returnAge = ageDiff - 1;
+          } else {
+            returnAge = ageDiff;
+          }
+        } else {
+          var monthDiff = nowMonth - birthMonth; //月之差
+          if (monthDiff < 0) {
+            returnAge = ageDiff - 1;
+          } else {
+            returnAge = ageDiff;
+          }
+        }
+      } else {
+        returnAge = -1; //返回-1 表示出生日期输入错误 晚于今天
+      }
     }
-  };
-
-  console.log(getSelectValue(list, "tom"), getSelectValue(list, "pp"));
-}
-{
-  var mySymbol = Symbol("oo");
-  console.log(mySymbol);
-
-  const obj = {
-    name: "tom",
-  };
-  for (let i in obj) {
-    console.log(i);
+    return returnAge; //返回周岁年龄
   }
-  for (let i of obj) {
-    console.log(i);
-  }
-}
-
-{
-  Number.prototype.say = () => {
-    console.log("hello");
-  };
-  const a = 1;
-  a.say();
+  console.log(getAgeByPid("20030905"));
 }
