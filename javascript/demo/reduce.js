a34aa77af29d87b8b85f5b24889397f9f006be06
drunk喵咪 <1684838553@qq.com>
2021-12-23 15:36:10 +0800

    Delete javascript directory

--- javascript/demo/reduce.js
+++
@@ -1,6 +0,0 @@
-var temp = [2, 4, 3];
-var total = temp.reduce((sum, item) => {
-  console.log(sum, item);
-  return sum + item;
-});
-console.log(total);
--- javascript/demo/time.js
+++
@@ -1,9 +0,0 @@
-const a = 200;
-
-const getTimeFormatter = (data) => {
-  const m = new Date(data).getMinutes().toString();
-  const s = new Date(data).getSeconds().toString();
-  return `${m.length === 1 ? 0 + m : m}:${s.length === 1 ? 0 + s : s}`;
-};
-
-getTimeFormatter(a);
